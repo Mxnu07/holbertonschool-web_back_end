6cb9e682738c28c4be0565cd5f79bb5b65adc0ad
MMD <manuelmoralesdiaz0@gmail.com>
2024-04-22 21:46:21 -0400

    adding union import

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """This module defines a function with type annotations."""
-from typing import List
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[int | float]) -> float:
-    """Sum a list of floats and integers."""
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """Sum a list of floats and integers"""
     return sum(mxd_lst)
--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,12 +1,8 @@
 #!/usr/bin/env python3
 """This module defines a function with type annotations."""
-from typing import Tuple
+from typing import Tuple, Union
 
 
-def to_kv(k: str, v: int | float) -> Tuple[str, float]:
-    """
-    Returns a tuple with the first element as
-    the input key and the second element as
-    the square of the input value.
-    """
-    return (k, (v**2))
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """Takes a string k and an int OR float v and returns a tuple."""
+    return (k, v * v)
