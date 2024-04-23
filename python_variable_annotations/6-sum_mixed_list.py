58a5e641645fbfe21b34755d3ae6048b3a1a19ea
MMD <manuelmoralesdiaz0@gmail.com>
2024-04-22 21:43:43 -0400

    fixing PEP8

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -4,5 +4,5 @@ from typing import List
 
 
 def sum_mixed_list(mxd_lst: List[int | float]) -> float:
-    """ """
+    """Sum a list of floats and integers."""
     return sum(mxd_lst)
--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
-""""""
+"""This module defines a function with type annotations."""
 from typing import Tuple
 
 
-
 def to_kv(k: str, v: int | float) -> Tuple[str, float]:
+    """
+    Returns a tuple with the first element as
+    the input key and the second element as
+    the square of the input value.
+    """
     return (k, (v**2))
