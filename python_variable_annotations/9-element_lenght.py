1c1780f8f8963344c5ea2900db8c38d2d1d676a0
MMD <manuelmoralesdiaz0@gmail.com>
2024-04-25 12:55:52 -0400

    DEBUGGING

--- python_variable_annotations/9-element_lenght.py
+++ python_variable_annotations/9-element_lenght.py
@@ -5,4 +5,4 @@ from typing import List, Tuple, Iterable, Sequence
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """Return a list of the lengths of the elements of lst."""
-    return [len(i) for i in lst]
+    return [(i, len(i)) for i in lst]
