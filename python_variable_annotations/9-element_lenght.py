1968024cea69f4d512fb2ba68cca7e9e7c12b2ee
MMD <manuelmoralesdiaz0@gmail.com>
2024-04-25 12:30:33 -0400

    debugging

--- python_variable_annotations/9-element_lenght.py
+++ python_variable_annotations/9-element_lenght.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """This module defines a function with type annotations."""
-from typing import List, Union
+from typing import List, Tuple, Iterable, Sequence
 
 
-def element_length(lst: List[Union[int, str]]) -> List[int]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """Return a list of the lengths of the elements of lst."""
     return [len(i) for i in lst]
