75c91652a7c58899e4023d1fcfd9ed5009c34567
MMD <manuelmoralesdiaz0@gmail.com>
2024-04-25 12:13:32 -0400

    PEP8

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -5,8 +5,7 @@ from typing import Callable, Union
 
 def make_multiplier(multiplier: float) -> Callable[[Union[int, float]], float]:
     """Returns a function that multiplies a float by multiplier."""
-   
-   
+
     def multiply(n: Union[int, float]) -> float:
         """Multiply n by multiplier."""
         return n * multiplier
