21c2f59152e84aa6f4faa467d684a3993f7c3736
MMD <manuelmoralesdiaz0@gmail.com>
2024-04-25 12:26:01 -0400

    PEP8

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -3,7 +3,7 @@
 from typing import Callable
 
 
-def make_multiplier(multiplier: float) -> Callable[[float],float]:
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """Returns a function that multiplies a float by multiplier."""
 
     def multiply(n: float):
