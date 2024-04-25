477872464f1c8739dac3a71572052285c6cb650d
MMD <manuelmoralesdiaz0@gmail.com>
2024-04-25 12:24:23 -0400

    debugging

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """This module defines a function with type annotations."""
-from typing import Callable, Union
+from typing import Callable
 
 
-def make_multiplier(multiplier: float) -> Callable[[Union[int, float]],
-                                                   float]:
+def make_multiplier(multiplier: float) -> Callable[[float],float]:
     """Returns a function that multiplies a float by multiplier."""
 
-    def multiply(n: Union[int, float]) -> float:
+    def multiply(n: float):
         """Multiply n by multiplier."""
         return n * multiplier
+    return multiply    
