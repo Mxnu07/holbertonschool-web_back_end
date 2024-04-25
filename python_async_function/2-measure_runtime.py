0c83865f2711912287fc13a01f4ca9f0d87a6676
MMD <manuelmoralesdiaz0@gmail.com>
2024-04-25 17:18:01 -0400

    2 and 3 tasks

---
+++ python_async_function/2-measure_runtime.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""This module defines a function that adds two float."""
+import asyncio
+import time
+
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+async def measure_time(n: int, max_delay: int) -> float:
+    ''' Function that returns the time '''
+    first = time.perf_counter()
+    asyncio.run(wait_n(n, max_delay))
+    finish = time.perf_counter()
+    total_time = finish - first
+    return total_time / n
