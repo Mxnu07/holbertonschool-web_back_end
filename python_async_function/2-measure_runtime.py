020f1ec15580ca01fa52e66df13ca2bbfa846c42
MMD <manuelmoralesdiaz0@gmail.com>
2024-04-25 19:07:28 -0400

    reverting more changes

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 """This module defines a function that adds two float."""
 import asyncio
-from time import perf_counter
-from typing import List
+import time
 
 
 wait_n = __import__('1-concurrent_coroutines').wait_n
@@ -10,8 +9,8 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 
 async def measure_time(n: int, max_delay: int) -> float:
     """Function that returns the time"""
-    first = perf_counter()
+    first = time.perf_counter()
     asyncio.run(wait_n(n, max_delay))
-    finish = perf_counter()
+    finish = time.perf_counter()
     total_time = finish - first
     return total_time / n
