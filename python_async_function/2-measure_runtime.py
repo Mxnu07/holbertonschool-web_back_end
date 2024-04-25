ba6faa91784526518ce38601f9b4fa9c62b51a3b
MMD <manuelmoralesdiaz0@gmail.com>
2024-04-25 18:00:10 -0400

    pep8

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -8,7 +8,7 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 async def measure_time(n: int, max_delay: int) -> float:
-    ''' Function that returns the time '''
+    """Function that returns the time"""
     first = time.perf_counter()
     asyncio.run(wait_n(n, max_delay))
     finish = time.perf_counter()
