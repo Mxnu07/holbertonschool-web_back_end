a5102dbe6ccc458930052a7be752f52039d7751d
MMD <manuelmoralesdiaz0@gmail.com>
2024-04-26 22:18:27 -0400

    importing union

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """ async comprehension """
 from typing import List
-ValueR = List[float]
+from typing import Union
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> ValueR:
+async def async_comprehension() -> List[Union[float, int]]:
     """ async comprehension """
     rnd = [float(i) async for i in async_generator()]
     return rnd
