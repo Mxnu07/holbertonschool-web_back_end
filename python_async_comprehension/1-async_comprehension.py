8be36d8e7ec4151461a7885fd116d7aeac05d0fd
MMD <manuelmoralesdiaz0@gmail.com>
2024-04-26 22:22:59 -0400

    changing random function

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 """ async comprehension """
 from typing import List
-from typing import Union
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> List[Union[float, int]]:
+async def async_comprehension() -> List[float]:
     """ async comprehension """
     rnd = [i async for i in async_generator()]
     return rnd
