71b4020199740ab7dc9f58bb7e85859f05803f25
MMD <manuelmoralesdiaz0@gmail.com>
2024-04-26 22:19:14 -0400

    removing float

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -8,5 +8,5 @@ async_generator = __import__('0-async_generator').async_generator
 
 async def async_comprehension() -> List[Union[float, int]]:
     """ async comprehension """
-    rnd = [float(i) async for i in async_generator()]
+    rnd = [i async for i in async_generator()]
     return rnd
