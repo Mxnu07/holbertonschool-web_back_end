8755cde7d87e4746edca9a92ecc95929620bd7e9
MMD <manuelmoralesdiaz0@gmail.com>
2024-04-26 20:59:51 -0400

    changing import

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 """ async comprehension """
 import asyncio
-from typing import List
+from typing import List, Vector
 import random
 
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> List[float]:
+async def async_comprehension() -> Vector[float]:
     """ async comprehension """
     rnd = [i async for i in async_generator()]
     return rnd
