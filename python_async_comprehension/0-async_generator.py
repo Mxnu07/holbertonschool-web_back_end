a87ffca01cef556d2e20531dada7b10e3d71d7cc
MMD <manuelmoralesdiaz0@gmail.com>
2024-04-26 22:00:21 -0400

    changin iterator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -7,6 +7,6 @@ import random
 
 async def async_generator() -> Generator[float, None, None]:
     """ async generator """
-    for i in range(10):
+    for _ in range(10):
         await asyncio.sleep(1)
         yield random.randint(0, 10)
