5af5f2764230346812e4736350a19f99a15b558f
MMD <manuelmoralesdiaz0@gmail.com>
2024-05-08 17:28:10 -0400

    6th task

--- ES6_basic/5-spread-operator.js
+++ ES6_basic/5-spread-operator.js
@@ -1,3 +1,3 @@
-export default function concatenateArrays(arr1, arr2, string) {
-    return [...arr1, ...arr2, ...string];
+export default function concatenateArrays(array1, array2, string) {
+    return [...array1, ...array2, ...string];
 }
