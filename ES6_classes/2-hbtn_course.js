62ceeddc15dee25a30628e064ba71a0ce0b9df8e
MMD <manuelmoralesdiaz0@gmail.com>
2024-05-10 20:23:55 -0400

    task #2

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,34 +1,51 @@
-export default class HolbertonCourse {
+class HolbertonCourse {
   constructor(name, length, students) {
     this.name = name;
     this.length = length;
     this.students = students;
   }
 
-  //   name
+  // name
   get name() {
     return this._name;
   }
 
-  set name(Name) {
-    this._name = Name;
+  set name(name) {
+    if (typeof name === 'string') {
+      this._name = name;
+    } else {
+      throw new TypeError('Name must be a string');
+    }
   }
 
-  //   length
+  // length
   get length() {
     return this._length;
   }
 
-  set length(Length) {
-    this._length = Length;
+  set length(length) {
+    if (typeof length === 'number') {
+      this._length = length;
+    } else {
+      throw new TypeError('Length must be a number');
+    }
   }
 
-  //   students
+  // students
   get students() {
     return this._students;
   }
 
-  set students(Students) {
-    this._students = Students;
+  set students(students) {
+    if (
+      Array.isArray(students)
+        && students.every((s) => typeof s === 'string')
+    ) {
+      this._students = students;
+    } else {
+      throw new TypeError('Students must be an array of strings');
+    }
   }
 }
+
+export default HolbertonCourse;
