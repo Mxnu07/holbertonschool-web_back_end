50b11482aa4f6c9dd3aa75f349ef3d84d0082ce5
MMD <manuelmoralesdiaz0@gmail.com>
2024-05-21 21:45:44 -0400

    task #2

---
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,12 @@
+function getStudentsByLocation(students, city) {
+  if (!Array.isArray(students)) {
+    return [];
+  }
+
+  const studentsByLocation = students.filter((item) => item.location === city);
+
+  return studentsByLocation;
+
+}
+
+export default getStudentsByLocation;
