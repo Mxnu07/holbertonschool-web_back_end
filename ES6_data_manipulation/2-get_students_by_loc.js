7c4fb791fbc089988f1c6a86406a647c4d0d7e26
MMD <manuelmoralesdiaz0@gmail.com>
2024-05-21 21:47:03 -0400

    fixing eslint

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,7 +1,7 @@
 function getStudentsByLocation(students, city) {
   if (!Array.isArray(students)) {
     return [];
-  }
+}
 
   const studentsByLocation = students.filter((item) => item.location === city);
 
