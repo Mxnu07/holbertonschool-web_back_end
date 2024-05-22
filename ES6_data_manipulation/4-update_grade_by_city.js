9caea4e77d5ecedeea173d8876fe34e4aee996ba
MMD <manuelmoralesdiaz0@gmail.com>
2024-05-22 10:54:02 -0400

    task #4

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,10 @@
+function updateStudentGradeByCity(students, city, newGrades) {
+  return students
+    .filter(student => student.city === city)
+    .map(student => {
+      const gradeObj = newGrades.find(grade => grade.studentId === student.id);
+      return { ...student, grade: gradeObj ? gradeObj.grade : 'N/A' };
+    });
+}
+
+export default updateStudentGradeByCity;
