39549cfd520beacd8134b4e7f13103032dbcd8ab
MMD <manuelmoralesdiaz0@gmail.com>
2024-05-24 20:22:54 -0400

    fixing mispelling

--- ES6_data_manipulation/9-groceries_list.js
+++ ES6_data_manipulation/9-groceries_list.js
@@ -1,7 +1,7 @@
 function groceriesList() {
   const groceries = new Map();
 
-  groceries.set('Apple', 10);
+  groceries.set('Apples', 10);
   groceries.set('Tomatoes', 10);
   groceries.set('Pasta', 1);
   groceries.set('Rice', 1);
