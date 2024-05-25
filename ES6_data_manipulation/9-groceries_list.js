f633ebfd8d6d8c21968f093f0fc85140082abd51
MMD <manuelmoralesdiaz0@gmail.com>
2024-05-24 20:09:32 -0400

    last tasks

---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,10 @@
+function groceriesList() {
+  const groceries = new Map();
+  groceries.set('apple', 5);
+  groceries.set('banana', 3);
+  groceries.set('milk', 2);
+
+  return groceries;
+}
+
+export default groceriesList;
